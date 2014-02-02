e859cf23b025cdbf9740855c348947265ebb96bc
César Rodríguez <kesarr@gmail.com>
2014-02-02 01:16:37 +0100

    skeleton for Jukebox kata

--- Katas/Jukebox/Jukebox.php
+++ Katas/Jukebox/Jukebox.php
@@ -4,5 +4,10 @@ namespace Katas\Jukebox;
 
 class Jukebox
 {
+    private $songs;
 
+    public function addSong(Song $song)
+    {
+        $this->songs[] = $song;
+    }
 }
--- Katas/Jukebox/Tests/JukeboxTest.php
+++ Katas/Jukebox/Tests/JukeboxTest.php
@@ -4,5 +4,19 @@ namespace Katas\Jukebox;
 
 class JukeboxTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Jukebox
+     */
+    public $jukebox;
 
+    public function setUp()
+    {
+        $this->jukebox = new Jukebox();
+    }
+
+    public function testAddSong()
+    {
+        $song_mock = $this->getMock('Katas\Jukebox\Song');
+        $this->jukebox->addSong($song_mock);
+    }
 }
